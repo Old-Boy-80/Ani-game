cc2d07b78ec8d2c358a3b770809415d203d065f6
Old-Boy-80 <2023bit052@sggs.ac.in>
2025-05-24 17:04:47 +0530

    refactor: update script.js with DOM manipulation for dynamic UI

--- src/public/script.js
+++ src/public/script.js
@@ -2,7 +2,8 @@ const startButton = document.getElementById('start-game');
 const howToPlayButton = document.getElementById('how-to-play');
 const closeButton = document.getElementById('close-btn');
 const instruction = document.getElementById('instruction');
-
+const game = document.getElementById('game');
+const description = document.getElementById("description");
  
 howToPlayButton.addEventListener('click', ()=> {
     instruction.classList.remove("opacity-0", "scale-0", "-z-10");
@@ -13,4 +14,10 @@ howToPlayButton.addEventListener('click', ()=> {
 closeButton.addEventListener('click', () => {
     instruction.classList.add("opacity-0", "scale-0", "-z-10");
     instruction.classList.remove("opacity-100", "z-10", "scale-1");
+})
+
+startButton.addEventListener('click', () => {
+    description.classList.toggle("hidden");
+    game.classList.remove("opacity-0", "scale-0", "h-0");
+    game.classList.add("opacity-100", "scale-1");
 })
